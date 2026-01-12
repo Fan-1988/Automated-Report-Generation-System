ee795f06af1c6991d0274ebde0cf34acf0fa6bd1
Fan-1988 <sijiafan6@gmail.com>
2026-01-12 11:55:12 -0800

    Add project files (secure config)

---
+++ flaskProject/config.py
@@ -0,0 +1,3 @@
+# 全局通用配置
+API_KEY = "sk-xxxxx"
+DB_URI = "mysql+pymysql://root:1234@localhost/tysql"
---
+++ login/src/views/ChartTest.vue
@@ -0,0 +1,13 @@
+<template>
+    <ChartTest/>
+</template>
+<script>
+import ChartTest from '../components/ChartReceiver.vue'
+
+export default ({
+   components:{
+    ChartTest
+   }
+})
+
+</script>
